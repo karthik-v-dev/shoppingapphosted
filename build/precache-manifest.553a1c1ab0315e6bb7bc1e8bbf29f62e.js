93d44016555246c29f9e9a5b974fe8a6be28fb51
MEGAMIND VIDEOS <karthikvoorugonda@gmail.com>
2020-04-20 10:38:43 +0530

    All Commit

---
+++ build/precache-manifest.553a1c1ab0315e6bb7bc1e8bbf29f62e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521e23bdcc293b190d8d124f0e77de26",
+    "url": "/shoppin-app/index.html"
+  },
+  {
+    "revision": "3f7efd10feb0940e438f",
+    "url": "/shoppin-app/static/css/main.264ecb6c.chunk.css"
+  },
+  {
+    "revision": "197c98e90f27d59b3961",
+    "url": "/shoppin-app/static/js/2.9e804755.chunk.js"
+  },
+  {
+    "revision": "fc9bf2d633f142820ff84625bb729377",
+    "url": "/shoppin-app/static/js/2.9e804755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7efd10feb0940e438f",
+    "url": "/shoppin-app/static/js/main.943567ba.chunk.js"
+  },
+  {
+    "revision": "3a662649589f6a4cbdf2",
+    "url": "/shoppin-app/static/js/runtime-main.b962b941.js"
+  },
+  {
+    "revision": "5e4b97d9d7a8f3be07375d2b0a25135b",
+    "url": "/shoppin-app/static/media/img1.5e4b97d9.jpg"
+  },
+  {
+    "revision": "b7a786ae124e953702d922396aecffb7",
+    "url": "/shoppin-app/static/media/img1.b7a786ae.jpeg"
+  },
+  {
+    "revision": "7651bdac6383106e825703de672de9f0",
+    "url": "/shoppin-app/static/media/img10.7651bdac.jpg"
+  },
+  {
+    "revision": "e9e7a6da8e6e9a1a4e22e7ed60ea307c",
+    "url": "/shoppin-app/static/media/img12.e9e7a6da.jpg"
+  },
+  {
+    "revision": "e5197867beeaea3db13f9eed853d01fb",
+    "url": "/shoppin-app/static/media/img13.e5197867.jpg"
+  },
+  {
+    "revision": "6c121c1ab4e01142fcf67b2216579427",
+    "url": "/shoppin-app/static/media/img3.6c121c1a.jpg"
+  },
+  {
+    "revision": "4736231d59d7a481f44f8709a5accc41",
+    "url": "/shoppin-app/static/media/img4.4736231d.jpg"
+  },
+  {
+    "revision": "fa404b71d1354e20c1c01e5ece3d789c",
+    "url": "/shoppin-app/static/media/img6.fa404b71.jpg"
+  },
+  {
+    "revision": "550dd5ae6931972b4151c8c046fcea79",
+    "url": "/shoppin-app/static/media/img7.550dd5ae.jpg"
+  },
+  {
+    "revision": "3801ec08936f8958e1d9df77e3cfacbb",
+    "url": "/shoppin-app/static/media/img8.3801ec08.jpg"
+  },
+  {
+    "revision": "7157988619d20b6be313f1c1c0bcec25",
+    "url": "/shoppin-app/static/media/img9.71579886.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppin-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8fc82fb99b393740f6497b2cc31b01e",
+    "url": "/shoppin-app/static/media/success.b8fc82fb.gif"
+  }
+]);
