d777b846a9e0b5ddbeaaa5c637bb7c5bff2d3297
MEGAMIND VIDEOS <karthikvoorugonda@gmail.com>
2020-04-20 10:53:05 +0530

    Updates

---
+++ precache-manifest.9fce593525c07fdc198e741fb4d3056b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa18ede50b6686fba1858953ef89e16",
+    "url": "/shoppingapphosted/index.html"
+  },
+  {
+    "revision": "398e0ec56d3d6fee41fe",
+    "url": "/shoppingapphosted/static/css/main.264ecb6c.chunk.css"
+  },
+  {
+    "revision": "808a92a5b8dadcf1b826",
+    "url": "/shoppingapphosted/static/js/2.18bd14e9.chunk.js"
+  },
+  {
+    "revision": "fc9bf2d633f142820ff84625bb729377",
+    "url": "/shoppingapphosted/static/js/2.18bd14e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398e0ec56d3d6fee41fe",
+    "url": "/shoppingapphosted/static/js/main.f045e400.chunk.js"
+  },
+  {
+    "revision": "92f441d6c3b15e9e13a5",
+    "url": "/shoppingapphosted/static/js/runtime-main.f2c0542d.js"
+  },
+  {
+    "revision": "5e4b97d9d7a8f3be07375d2b0a25135b",
+    "url": "/shoppingapphosted/static/media/img1.5e4b97d9.jpg"
+  },
+  {
+    "revision": "b7a786ae124e953702d922396aecffb7",
+    "url": "/shoppingapphosted/static/media/img1.b7a786ae.jpeg"
+  },
+  {
+    "revision": "7651bdac6383106e825703de672de9f0",
+    "url": "/shoppingapphosted/static/media/img10.7651bdac.jpg"
+  },
+  {
+    "revision": "e9e7a6da8e6e9a1a4e22e7ed60ea307c",
+    "url": "/shoppingapphosted/static/media/img12.e9e7a6da.jpg"
+  },
+  {
+    "revision": "e5197867beeaea3db13f9eed853d01fb",
+    "url": "/shoppingapphosted/static/media/img13.e5197867.jpg"
+  },
+  {
+    "revision": "6c121c1ab4e01142fcf67b2216579427",
+    "url": "/shoppingapphosted/static/media/img3.6c121c1a.jpg"
+  },
+  {
+    "revision": "4736231d59d7a481f44f8709a5accc41",
+    "url": "/shoppingapphosted/static/media/img4.4736231d.jpg"
+  },
+  {
+    "revision": "fa404b71d1354e20c1c01e5ece3d789c",
+    "url": "/shoppingapphosted/static/media/img6.fa404b71.jpg"
+  },
+  {
+    "revision": "550dd5ae6931972b4151c8c046fcea79",
+    "url": "/shoppingapphosted/static/media/img7.550dd5ae.jpg"
+  },
+  {
+    "revision": "3801ec08936f8958e1d9df77e3cfacbb",
+    "url": "/shoppingapphosted/static/media/img8.3801ec08.jpg"
+  },
+  {
+    "revision": "7157988619d20b6be313f1c1c0bcec25",
+    "url": "/shoppingapphosted/static/media/img9.71579886.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingapphosted/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8fc82fb99b393740f6497b2cc31b01e",
+    "url": "/shoppingapphosted/static/media/success.b8fc82fb.gif"
+  }
+]);
